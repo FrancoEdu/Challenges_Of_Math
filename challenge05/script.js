fa1552b07c10938de9a6101c5e62211c2b078b31
FrancoEdu <franc0edu24@gmail.com>
2023-02-02 22:03:45 -0300

    Finished exercise 05

--- challenge05/script.js
+++ challenge05/script.js
@@ -1,10 +1,27 @@
 let assinante = [true, false, false, false, true, false,true,true, false, false, false]
 let minutos_assistidos = [6144, 2742, 330, 30, 1500, 4032, 24036, 3288, 2076, 24540, 4716]
 let horasVet = []
+let totalHorasAssistidas = 0;
+let porcentagem;
+let porcentagemVet = [];
 
 minutos_assistidos.forEach(minutos => {
     let horas_assistidas = Math.ceil(minutos/60)
     horasVet.push(horas_assistidas)
 });
+console.log(horasVet)
+for(let x = 0; x < assinante.length; x++){
+    if(assinante[x] == true){
+        horasVet[x] = horasVet[x] * 2
+        totalHorasAssistidas = totalHorasAssistidas + horasVet[x]
+    }else{
+        totalHorasAssistidas = totalHorasAssistidas + horasVet[x]
+    }
+}
 
-for()
+for(let x = 0; x<horasVet.length;x++){
+    porcentagem = Math.ceil(((horasVet[x] * 100)/totalHorasAssistidas))
+    porcentagemVet.push(porcentagem)
+}
+
+console.log(porcentagemVet)
